b07d3e8b569b233d3af7aca72dd8c39dd56fd7a8
paolajose <paoj398@gmail.com>
2021-01-12 16:25:32 -0400

    formulario

---
+++ funcionalidad.js
@@ -0,0 +1,12 @@
+let contenido = document.getElementById("contenido")
+
+let formulario = document.getElementById("formulario")
+
+formulario.addEventListener("submit" , function(e){
+    e.preventDefault();
+
+
+    const data = (document.getElementById("formulario"))
+
+    console.log("data")
+})
